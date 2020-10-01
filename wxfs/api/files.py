b3bf00996bc28036b2c59758e383d3d0c79c4887
rod-glover <rod.glover.ca@gmail.com>
2020-09-30 19:57:48 -0700

    Implement API against database

--- wxfs/api/files.py
+++ wxfs/api/files.py
@@ -1,9 +1,82 @@
+from flask import url_for
+from wxfs.database import WxFile, SummaryFile
+from wxfs import get_app_session
+
+
+def uri(file):
+    return url_for('.wxfs_api_files_get', id=file.id)
+
+
+def single_item_rep(file):
+    """Return representation of a single file item."""
+    rep_common = {
+        "id": file.id,
+        "selfUri": uri(file),
+        "fileType": file.fileType,
+        "filepath": file.filepath,
+        "contentUri": "TBD",
+    }
+    if file.fileType == "weather":
+        return {
+            **rep_common,
+            "creationDate": file.creationDate,
+            "dataSource": file.dataSource,
+            "designDataType": file.designDataType,
+            "scenario": file.scenario,
+            "timePeriod": {
+                "start": file.timePeriodStart,
+                "end": file.timePeriodEnd,
+            },
+            "ensembleStatistic": file.ensembleStatistic,
+            "variables": file.variables,
+            "anomaly": file.anomaly,
+            "smoothing": file.smoothing,
+        }
+    elif file.fileType == "summary":
+        return rep_common
+    else:
+        raise ValueError(f"Invalid file type: {file.fileType}")
+
+
+def collection_item_rep(file):
+    """Return representation of a file collection item.
+    May conceivably be different than representation of a single a file.
+    """
+    return single_item_rep(file)
+
+
+def collection_rep(files):
+    """Return representation of files collection. """
+    return [collection_item_rep(file) for file in files]
+
+
 def listing():
-    return []
+    session = get_app_session()
+    # TODO: Find out if there is a way to query on File only and
+    #  extend appropriately according to discriminator
+    wx_files = (
+        session
+            .query(WxFile)
+            .order_by(WxFile.id.asc())
+            .all()
+    )
+    summary_files = (
+        session
+            .query(SummaryFile)
+            .order_by(SummaryFile.id.asc())
+            .all()
+    )
+    return collection_rep(wx_files + summary_files)
 
 
-def get():
-    return {}
+def get(id):
+    file = (
+        get_app_session()
+            .query(File)
+            .filter_by(id=id)
+            .one()
+    )
+    return single_item_rep(file)
 
 
 def getContent():
--- wxfs/api/locations.py
+++ wxfs/api/locations.py
@@ -1,9 +1,59 @@
-from wxfs.fake_data import locations
+from flask import url_for
+from wxfs.database import Location
+from wxfs import get_app_session
+from wxfs.api.files import collection_rep as files_collection_rep
+
+
+def uri(location):
+    return url_for('.wxfs_api_locations_get', id=location.id)
+
+
+def single_item_rep(location):
+    """Return representation of a single location item."""
+    return {
+        "id": location.id,
+        "selfUri": uri(location),
+        "city": location.city,        
+        "province": location.province,        
+        "country": location.country,        
+        "code": location.code,        
+        "latitude": location.latitude,        
+        "longitude": location.longitude,        
+        "elevation": location.elevation,
+        "files": (
+            files_collection_rep(location.wx_files) +
+            files_collection_rep(location.summary_files)
+        )
+    }
+
+
+def collection_item_rep(location):
+    """Return representation of a location collection item.
+    May conceivably be different than representation of a single a location.
+    """
+    return single_item_rep(location)
+
+
+def collection_rep(locations):
+    """Return representation of locations collection. """
+    return [collection_item_rep(location) for location in locations]
 
 
 def listing():
-    return locations
+    locations = (
+        get_app_session()
+            .query(Location)
+            .order_by(Location.id.asc())
+            .all()
+    )
+    return collection_rep(locations)
 
 
 def get(id=None):
-    return locations[id]
+    location = (
+        get_app_session()
+            .query(Location)
+            .filter_by(id=id)
+            .one()
+    )
+    return single_item_rep(location)
